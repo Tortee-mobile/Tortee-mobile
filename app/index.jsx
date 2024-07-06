9162e2b019950af15b48b06d876e6a8e16360e7a
supernhilty <nhilty302814@gmail.com>
2024-07-06 12:54:28 +0700

    [Home][Nhi] add home UI and add resource

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,68 @@
-import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { CustomButton, Loader } from "../components";
+import { images } from "../constants";
 
-export default function App() {
+//import { useGlobalContext } from "../context/GlobalProvider";
+
+const Welcome = () => {
+  //const { loading, isLogged } = useGlobalContext();
+
+  // if (!loading && isLogged) return <Redirect href="/home" />;
+
   return (
-    <View className="flex-1 items-center justify-center ">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" className="text-blue-500">
-        Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      {/* <Loader isLoading={loading} /> */}
+
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discovery Mentorship opportunities with{"\n"}{" "}
+              <Text className="text-blue-200">Tortee</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Seeking to learn, grow, and connect in their professional and
+            personal development journeys with Tortee
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
-}
+};
+
+export default Welcome;
---
+++ constants/images.js
@@ -0,0 +1,6 @@
+//import profile from "../assets/images/profile.png";
+
+import cards from "../assets/images/cards.png";
+import logo from "../assets/images/logo.png";
+
+export default { cards, logo };
